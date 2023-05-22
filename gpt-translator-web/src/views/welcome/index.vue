1c8edae74ddaa07db0fa17cec970c8edbab9398b
luoshuzhong <zdthm2010@gmail.com>
2023-05-22 09:33:46 +0800

    首页简介修改

--- gpt-translator-web/src/views/welcome/index.vue
+++ gpt-translator-web/src/views/welcome/index.vue
@@ -7,5 +7,6 @@ defineOptions({
 <template>
   <div>
     <h1>这是一个根据MTool和T++导出其原文用GPT翻译后输出的项目</h1>
+    <a href="https://github.com/XHXJ/json-GPT-translator" style="color:#0034ff;">github地址</a>
   </div>
 </template>
